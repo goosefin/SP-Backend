7964ef912fa011e2b6b1f5155df1bb47c673eae0
goosefin <agustinafin96@gmail.com>
2023-06-09 20:18:31 -0400

    controllers refactored into async/await format

--- controllers/review.ctrls.js
+++ controllers/review.ctrls.js
@@ -1,5 +1,33 @@
 const db = require('../models')
-
-// REVIEW CTRLS
+const session = require('express-session')
 
 //Create
+const create = async (req,res) =>{
+    try{
+        const newReview = await db.Review.create(req.body)
+        console.log('New Review: ', newReview)
+        return res.status(200).json({review:newReview})
+    } catch(err){
+        console.log(db.User.reviews)
+        return res.status(404).json({error:err.message})
+    }
+}
+
+//Delete
+const destroy = async (req,res) =>{
+    try{
+        const foundReview = await db.Review.findByIdAndDelete(req.params.id)
+        if(!foundReview){
+            return res.status(400).json({error: 'Review not found'})
+        }else{
+            return res.status(200).json({message:`${foundReview.id} deleted successfully`}) 
+        }
+    }catch(err){
+        return res.status(404).json({error:err.message})
+    }
+}
+
+module.exports = {
+    create,
+    destroy
+}
--- controllers/spotussy.ctrls.js
+++ controllers/spotussy.ctrls.js
@@ -1,5 +1,8 @@
 const db = require('../models')
+const session = require('express-session')
+const res = require('express/lib/response')
 
+//Index
 const index = async (req,res) =>{
     try {
         const allSpots = await db.Spotussy.find({})
@@ -12,6 +15,33 @@ const index = async (req,res) =>{
     }
 }
 
+//Create
+const create = async (req,res) =>{
+    try{
+        const newSpot = await db.Spotussy.create(req.body)
+        console.log('New Spotussy: ', newSpot)
+        return res.status(200).json({spotussy:newSpot})
+    } catch(err){
+        return res.status(404).json({error:err.message})
+    }
+}
+
+//Delete
+const destroy = async (req,res) =>{
+    try{
+        const oneSpot = await db.Spotussy.findByIdAndDelete(req.params.id)
+        if(!oneSpot){
+            return res.status(400).json({error: 'Spot not found'})
+        }else{
+            return res.status(200).json({message:`${oneSpot.id} deleted successfully`}) 
+        }
+    }catch(err){
+        return res.status(404).json({error:err.message})
+    }
+}
+
 module.exports = {
-    index
+    index,
+    create,
+    destroy
 }

3451d8dbc929ec4943ea6bec0bd5432ec1b891c7
goosefin <agustinafin96@gmail.com>
2023-07-13 20:18:09 -0400

    debugging heroku connection

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/SPDB'
+const connectionStr = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SPDB'
 
 mongoose.connect(connectionStr)
 
